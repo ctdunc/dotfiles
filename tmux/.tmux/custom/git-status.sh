c085226fcdf0593524337c0bd99c8d3f3678c1fa
Connor Duncan <connor.duncan@invesco.com>
2024-08-09 09:10:25 -0500

    Add git-status waypoint

---
+++ tmux/.tmux/custom/git-status.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd $(tmux display-message -p -F "#{pane_current_path}")
+cd $PANE_PATH
+
+git_changes() {
+	local changes=$(git diff --shortstat | sed -E -e 's/[^0-9|,]//g' | awk -F"," '{print "~"$1"  +"$2"  -"$3}')
+	local untracked=$(git status --porcelain 2>/dev/null | grep -c "^??")
+	echo $changes  ?$untracked
+}
+
+git_changes
