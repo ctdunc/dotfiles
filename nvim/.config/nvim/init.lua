c95b91c59b674a864b0a12701cae6887e191eceb
Connor Duncan <connor@connorduncan.xyz>
2024-12-16 07:30:36 -0600

    Format and clean up nvim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -101,14 +101,14 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagn
 --
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
 
 -- Add lazy to the `runtimepath`, this allows us to `require` it.
@@ -117,8 +117,8 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Set up lazy, and load my `lua/custom/plugins/` folder
 require("lazy").setup({ import = "custom/plugins" }, {
-	change_detection = {
-		notify = false,
-	},
+  change_detection = {
+    notify = false,
+  },
 })
 vim.cmd([[colorscheme gruvbox]])
--- nvim/.config/nvim/lua/custom/plugins/dadbod.lua
+++ nvim/.config/nvim/lua/custom/plugins/dadbod.lua
@@ -1,8 +1,5 @@
 return {
-	-- { dir = "/home/connor/code/dadbod/vim-dadbod-snowflake" },
-	-- { dir = "/home/connor/code/dadbod/vim-dadbod-completion" },
-	-- { dir = "/home/connor/code/dadbod/vim-dadbod-ui" },
-	"tpope/vim-dadbod",
-	"kristijanhusak/vim-dadbod-completion",
-	"kristijanhusak/vim-dadbod-ui",
+  "tpope/vim-dadbod",
+  "kristijanhusak/vim-dadbod-completion",
+  "kristijanhusak/vim-dadbod-ui",
 }
