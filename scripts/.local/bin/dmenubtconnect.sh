2d3000cb68539cab75a99619dd439e7e7782b7cd
Connor Duncan <connor@connorduncan.xyz>
2026-01-18 08:56:42 -0600

    Minor weather changes

--- scripts/.local/bin/dmenubtconnect.sh
+++ scripts/.local/bin/dmenubtconnect.sh
@@ -1,3 +1,4 @@
 SELECTED=$(bluetoothctl devices Paired | cut -d' ' -f3 | dmenu "$@" -i -p "Connect BT Device:")
 SELECTED_ID=$(bluetoothctl devices Paired | grep "\s$SELECTED$" | cut -d' ' -f2)
+echo $SELECTED_ID
 bluetoothctl connect $SELECTED_ID
