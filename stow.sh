636d8eec9d1313945ed38f63093d465d9f23f895
Connor Duncan <connor@connorduncan.xyz>
2026-05-17 15:12:54 -0500

    re-add gtk theme

--- stow.sh
+++ stow.sh
@@ -4,8 +4,8 @@ stow --target=$HOME tmux
 stow --target=$HOME scripts
 stow --target=$HOME dunst
 stow --target=$HOME brain
-stow --target=$HOME gtk-gruv
 stow --target=$HOME neomutt
 stow --target=$HOME systemd
+stow --target=$HOME gtk
 # stow no-track, environment variables I don't want version controlled.
 stow --target=$HOME no-track
